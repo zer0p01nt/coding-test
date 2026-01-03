9d9076c34237be11f086d8d5b5c2655a60f85e95
Baek MY <1027.bmy@gmail.com>
2026-01-03 19:23:48 +0900

    [Bronze II] Title: 다이얼, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+
+const inputArray = fs.readFileSync("/dev/stdin").toString().trim().split("");
+
+let time = 0;
+
+inputArray.map((char) => {
+  switch (char) {
+    case "A":
+    case "B":
+    case "C":
+      time += 3;
+      break;
+    case "D":
+    case "E":
+    case "F":
+      time += 4;
+      break;
+    case "G":
+    case "H":
+    case "I":
+      time += 5;
+      break;
+    case "J":
+    case "K":
+    case "L":
+      time += 6;
+      break;
+    case "M":
+    case "N":
+    case "O":
+      time += 7;
+      break;
+    case "P":
+    case "Q":
+    case "R":
+    case "S":
+      time += 8;
+      break;
+    case "T":
+    case "U":
+    case "V":
+      time += 9;
+      break;
+    case "W":
+    case "X":
+    case "Y":
+    case "Z":
+      time += 10;
+      break;
+    default:
+      break;
+  }
+});
+
+console.log(time);
