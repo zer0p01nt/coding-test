d23288f00aaa690f1fe98a739425f75548712d84
Baek MY <1027.bmy@gmail.com>
2026-01-15 01:50:01 +0900

    [level 0] Title: 배열 만들기 6, Time: 10.05 ms, Memory: 63.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+    let i = 0;
+    let stk = []
+    while (i < arr.length) {
+        if (stk.length === 0) {
+            stk.push(arr[i])
+        } else if (stk.length > 0 && stk[stk.length-1] === arr[i]) {
+            stk.pop()
+        } else {
+            stk.push(arr[i])
+        }
+        i++
+    }
+    return stk.length ? stk : [-1]
+}
