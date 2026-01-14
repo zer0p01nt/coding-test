ed57a5cc3580a4dc0d91a620f4cc43dd32657d67
Baek MY <1027.bmy@gmail.com>
2026-01-15 01:41:08 +0900

    [level 0] Title: 세 개의 구분자, Time: 13.98 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,8 +1,3 @@
 function solution(myStr) {
-    const strAry = myStr.split("").map((s) => {
-        if (s === "a" || s === "b" || s === "c") return " "
-        else return s
-    }).join("").trim().split(" ").filter((s) => s.length > 0)
-    
-    return strAry.length === 0 ? ["EMPTY"] : strAry
+    return myStr.match(/[^a-c]+/g) || ['EMPTY'];;
 }
