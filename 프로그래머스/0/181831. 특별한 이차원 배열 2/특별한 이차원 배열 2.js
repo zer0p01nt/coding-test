825bd120650d81587f30ac53dbb1aed4d58868e2
Baek MY <1027.bmy@gmail.com>
2026-01-18 00:45:05 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 26.40 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    let n = arr.length
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (arr[i][j] !== arr[j][i]) {
+                return 0
+            }
+        }
+    }
+    return 1
+}
