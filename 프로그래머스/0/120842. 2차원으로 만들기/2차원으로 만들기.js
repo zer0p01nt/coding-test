80106a19c19e19ec5d3413f7efd48098d39f9b84
Baek MY <1027.bmy@gmail.com>
2026-01-23 09:51:03 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,13 +1,9 @@
 function solution(num_list, n) {
     let answer = [];
-    const len = num_list.length / n
-    for (let i = 0; i < len; i++) {
-        let arr = []
-        while (true) {
-            arr.push(num_list.shift())
-            if (arr.length === n) break;
-        }
-        answer.push(arr)
+
+    while(num_list.length) {
+        answer.push(num_list.splice(0,n));
     }
+
     return answer;
 }
