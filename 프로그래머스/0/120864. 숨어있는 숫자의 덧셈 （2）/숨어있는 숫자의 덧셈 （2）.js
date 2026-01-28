01ba8e6cc090ddbbbdf75b185f87db5d5567b492
Baek MY <1027.bmy@gmail.com>
2026-01-29 02:11:34 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,4 +1,3 @@
 function solution(my_string) {
-    const arr = my_string.replace(/[^0-9]/g, 'x').split("x").filter(Boolean)
-    return arr.length === 0 ? 0 : arr.length === 1 ? +arr[0] : arr.reduce((a, c) => +a + +c)
+    return my_string.split(/\D+/).reduce((acc, cur) => acc + Number(cur), 0);
 }
