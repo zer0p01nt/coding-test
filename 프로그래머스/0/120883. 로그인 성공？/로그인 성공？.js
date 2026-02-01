596b0e9e911929029d5e6b4a131e7a32047d0ed9
Baek MY <1027.bmy@gmail.com>
2026-02-02 01:59:59 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,15 +1,5 @@
 function solution(id_pw, db) {
-    let arr = []
-    db.map((l) => {
-        if (l[0] === id_pw[0]) {
-            if (l[1] === id_pw[1]) {
-                arr.push("login")
-            } else {
-                arr.push("wrong")
-            }
-        } else {
-            arr.push("fail")
-        }
-    })
-    return arr.includes("login") ? "login" : arr.includes("wrong") ? "wrong pw" : "fail"
+    const [id, pw] = id_pw;
+    const map = new Map(db);
+    return map.has(id) ? (map.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
 }
