33a70b756dda24a496fc866c3adc34fb0b0bc6a8
Baek MY <1027.bmy@gmail.com>
2026-02-05 00:25:10 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,16 +1,3 @@
 function solution(a, b) {
-    let sum = 0;
-    
-    if (a === b) {
-        return a
-    }
-    
-    else if (a > b) {
-        [a, b] = [b, a]
-    }
-    
-    for (let i = a; i <= b; i++) {
-        sum += i
-    }
-    return sum
+    return (a+b) * (Math.abs(a-b)+1) / 2;
 }
