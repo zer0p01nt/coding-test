652503fd0da91ddaf21a5ae92fff1e10e5d2fd3c
Baek MY <1027.bmy@gmail.com>
2026-02-06 02:28:32 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -2,6 +2,6 @@ function solution(n) {
     if (n % 2 === 0) {
         return "수박".repeat(n/2)
     } else {
-        return "수박".repeat(Math.floor(n/2)) + "수"
+        return "수박".repeat(n/2) + "수"
     }
 }
