f49d3365dfc99cafcb6ace6020c050b630a96f68
Baek MY <1027.bmy@gmail.com>
2026-02-06 02:32:33 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,16 +1,10 @@
 function solution(left, right) {
     let result = 0;
     for (let i = left; i <= right; i++) {
-        let sum = 0;
-        for (let j = 1; j <= i; j++) {
-            if (i % j === 0) {
-                sum++
-            }
-        }
-        if (sum % 2 === 0) {
-            result += i
+        if (Number.isInteger(Math.sqrt(i))) {
+            result -= i;
         } else {
-            result -= i
+            result += i;
         }
     }
     return result
