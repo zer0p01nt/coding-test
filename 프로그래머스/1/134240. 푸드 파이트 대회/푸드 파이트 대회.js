64ad302ce914b9b84a160590537a721b33dacadb
Baek MY <1027.bmy@gmail.com>
2026-02-10 11:50:50 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,23 +1,10 @@
 function solution(food) {
-    let a = []
-    let b = []
-    
-    food.map((n, i) => {
-        if (i === 0) {
-            return
-        } else {
-            let amount = Math.trunc(n / 2)
-            a.push(String(i).repeat(amount))
-        }
-    })
-    food.map((n, i) => {
-        if (i === 0) {
-            return
-        } else {
-            let amount = Math.trunc(n / 2)
-            b.unshift(String(i).repeat(amount))
+    let arr = food.map((n, i) => {
+        if (i !== 0) {
+            let amount = Math.trunc(n/2)
+            return String(i).repeat(amount)
         }
     })
     
-    return [...a, 0, ...b].join("")
+    return arr.join("") + "0" + arr.reverse().join("")
 }
