4249ac67172cb6d399f8ff75b70a5a714b5f582b
Baek MY <1027.bmy@gmail.com>
2026-02-16 01:47:32 +0900

    [Bronze V] Title: A×B, Time: 84 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath =
+  process.platform === "linux"
+    ? "/dev/stdin"
+    : path.join(__dirname, "input.txt");
+
+const input = fs.readFileSync(filePath).toString().trim();
+// const inputArr = input === "" ? [] : input.split(/\s+/);
+const inputArr = input === "" ? [] : input.split(" ");
+
+const a = parseInt(inputArr[0]);
+const b = parseInt(inputArr[1]);
+
+console.log(a * b);
