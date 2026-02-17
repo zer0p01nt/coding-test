aff81d84f409736be0d8b96298796bfd1885aa7c
Baek MY <1027.bmy@gmail.com>
2026-02-17 20:56:40 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 88 ms, Memory: 9504 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath =
+  process.platform === "linux" ? 0 : path.join(__dirname, "input.txt");
+
+const input = fs.readFileSync(filePath).toString().trim();
+const inputArr = input === "" ? [] : input.split(/\s+/);
+
+for (let i = 1; i <= inputArr[0]; i++) {
+  console.log("*".repeat(i));
+}
