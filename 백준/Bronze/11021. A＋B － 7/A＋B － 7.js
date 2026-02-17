b288cf4b67d74e6c34d22eed8ea3ac934ef69c52
Baek MY <1027.bmy@gmail.com>
2026-02-17 20:26:07 +0900

    [Bronze V] Title: A+B - 7, Time: 88 ms, Memory: 9516 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath =
+  process.platform === "linux" ? 0 : path.join(__dirname, "input.txt");
+
+const input = fs.readFileSync(filePath).toString().trim();
+const inputArr = input === "" ? [] : input.split(/\s+/);
+
+let cnt = 1;
+for (let i = 1; i < inputArr.length; i += 2) {
+  console.log(`Case #${cnt++}: ${+inputArr[i] + +inputArr[i + 1]}`);
+}
