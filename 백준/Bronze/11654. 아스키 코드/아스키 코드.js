ce62f0b156ccfe57bf987397a3bc22ebb221d766
Baek MY <1027.bmy@gmail.com>
2026-02-20 02:14:38 +0900

    [Bronze V] Title: 아스키 코드, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath =
+  process.platform === "linux" ? 0 : path.join(__dirname, "input.txt");
+
+const input = fs.readFileSync(filePath).toString().trim();
+
+console.log(input.charCodeAt(0));
