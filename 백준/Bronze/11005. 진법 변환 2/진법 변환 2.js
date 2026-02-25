cd987a225db0c6d8a4dcbd8d33da7343d84abae2
Baek MY <1027.bmy@gmail.com>
2026-02-25 23:08:42 +0900

    [Bronze I] Title: 진법 변환 2, Time: 148 ms, Memory: 9620 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath =
+  process.platform === "linux" ? 0 : path.join(__dirname, "input.txt");
+
+const input = fs.readFileSync(filePath).toString().trim();
+const inputArr = input === "" ? [] : input.split(/\s+/);
+
+const n = +inputArr[0];
+const b = +inputArr[1];
+
+console.log(n.toString(b).toUpperCase());
